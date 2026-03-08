abd40403e2ab0626e4af4a9bb32ccf1dcb4c43ad
Kyle Schaudt <khs2p3@mail.umkc.edu>
2026-03-07 19:42:01 -0700

    login button

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,13 +1,14 @@
 import { redirectToSpotify } from "../../auth/redirectToSpotify";
+import Button from "../../components/button/Button";
 import "./Login.css";
 
 export default function Login() {
   return (
     <div className="login-wrapper">
       <h2>Login</h2>
-      <button style={{ marginTop: "16px" }} onClick={() => redirectToSpotify()}>
+      <Button style={{ marginTop: "16px" }} onClick={() => redirectToSpotify()}>
         Login to Spotify
-      </button>
+      </Button>
     </div>
   );
 }
